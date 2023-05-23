e8dc5805f8a735ec853827d12f2615bf4d70fc87
Murilo <murilorez79@hotmail.com>
2023-05-23 18:59:03 -0300

    Adição do dotenv no projeto

--- src/models/AccountData.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const AccountSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-});
-
-module.exports = mongoose.model('Accounts', AccountSchema);
